4db4dfdc391c06a0233b439e46182c07234c849d
Hwangsoobin-a <61505435+Hwangsoobin-a@users.noreply.github.com>
2020-06-09 16:58:28 +0900

    add join()

--- src/main/java/edu/handong/csee/java/examples/thread/lab/SumMultipleThreads.java
+++ src/main/java/edu/handong/csee/java/examples/thread/lab/SumMultipleThreads.java
@@ -10,7 +10,7 @@ public class SumMultipleThreads implements Runnable{
 
 		long to = 10000000;
 		ArrayList<SumMultipleThreads> sumRunners = new ArrayList<SumMultipleThreads>();
-
+		//ArrayList<SumMultipleThreads> toRuns = new ArrayList<SumMultipleThreads>();
 		/* Let a thread compute a sub-sum.
 		 * sumRunners.add(new SumMultipleThreads(1,1000000));
 		 * sumRunners.add(new SumMultipleThreads(1000001,2000000));
@@ -22,15 +22,20 @@ public class SumMultipleThreads implements Runnable{
 		 * sumRunners.add(new SumMultipleThreads(7000001,8000000));
 		 * sumRunners.add(new SumMultipleThreads(8000001,9000000));
 		 * sumRunners.add(new SumMultipleThreads(9000001,10000000));*/
+		
 		for(long i=0; i<to/1000000; i++) {
 			sumRunners.add(new SumMultipleThreads((i*1000000)+1, (i+1)*1000000));
 		}
-
+		
 		ArrayList<Thread> threadsForSubSum = new ArrayList<Thread>();
 
 		for(SumMultipleThreads runner:sumRunners) {
 			Thread thread = new Thread(runner);
+			
 			thread.start();
+			try {thread.join();
+	         }catch(Exception e) {
+	         }
 			threadsForSubSum.add(thread);
 		}
 
